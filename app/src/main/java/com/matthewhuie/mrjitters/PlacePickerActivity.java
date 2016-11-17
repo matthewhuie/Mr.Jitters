50224ca9631d3b15ac4d7d41c8746fdddfe7aa24
Matthew Huie <mhuie@foursquare.com>
2016-11-16 20:14:31 -0500

    Added PlacePickerActivity

--- app/src/main/java/com/matthewhuie/mrjitters/PlacePickerActivity.java
+++ app/src/main/java/com/matthewhuie/mrjitters/PlacePickerActivity.java
@@ -1,26 +1,18 @@
 package com.matthewhuie.mrjitters;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.transition.Slide;
-import android.transition.TransitionInflater;
-import android.view.View;
 
-public class MainActivity extends Activity {
+public class PlacePickerActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_place_picker);
 
         Slide slide = new Slide();
         slide.setDuration(1000);
         getWindow().setExitTransition(slide);
-    }
-
-    public void gotoPlacePicker(View view) {
-        Intent i = new Intent(getApplicationContext(), PlacePickerActivity.class);
-        startActivity(i);
     }
 }
