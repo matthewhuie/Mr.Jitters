261ce3be5ff6d085ba331095dbb4e6ea26547160
Matthew Huie <mhuie@foursquare.com>
2016-11-17 20:47:33 -0500

    Lots of updates - FoursquareService - Location via Google APIs

--- app/src/main/java/com/matthewhuie/mrjitters/MainActivity.java
+++ app/src/main/java/com/matthewhuie/mrjitters/MainActivity.java
@@ -3,52 +3,22 @@ package com.matthewhuie.mrjitters;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.transition.Slide;
+import android.transition.TransitionInflater;
+import android.view.Gravity;
 import android.view.View;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
-import android.view.animation.RotateAnimation;
-import android.widget.ImageView;
+import android.view.Window;
 
-public class MainActivity extends Activity implements View.OnClickListener {
-
-    private ImageView logo;
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        logo = (ImageView)findViewById(R.id.mrjitters);
-        logo.setOnClickListener(this);
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        RotateAnimation jitter = new RotateAnimation(0, 2, 50, 50);
-        jitter.setDuration(10);
-        jitter.setRepeatCount(Animation.INFINITE);
-        jitter.setRepeatMode(Animation.REVERSE);
-        logo.startAnimation(jitter);
-    }
-
-    @Override
-    public void onClick(View view) {
-        Animation click = AnimationUtils.loadAnimation(this, R.anim.click);
-        click.setAnimationListener(new Animation.AnimationListener() {
-
-            @Override
-            public void onAnimationStart(Animation animation) {}
-
-            @Override
-            public void onAnimationRepeat(Animation animation) {}
-
-            @Override
-            public void onAnimationEnd(Animation animation) {
-                Intent i = new Intent(getApplicationContext(), PlacePickerActivity.class);
-                startActivity(i);
-            }
-        });
-        view.startAnimation(click);
+    public void gotoPlacePicker(View view) {
+        Intent i = new Intent(getApplicationContext(), PlacePickerActivity.class);
+        startActivity(i);
     }
 }
