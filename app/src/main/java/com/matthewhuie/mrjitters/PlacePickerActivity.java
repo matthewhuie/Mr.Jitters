7bb7adc93979b1c89dbdf37ee32c124a5744cae7
Matthew Huie <mhuie@foursquare.com>
2016-11-20 13:55:30 -0500

    Minor code cleanup

--- app/src/main/java/com/matthewhuie/mrjitters/PlacePickerActivity.java
+++ app/src/main/java/com/matthewhuie/mrjitters/PlacePickerActivity.java
@@ -17,7 +17,6 @@ import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationServices;
 
-import java.util.Iterator;
 import java.util.List;
 
 import retrofit2.Call;
@@ -53,6 +52,16 @@ public class PlacePickerActivity extends Activity implements GoogleApiClient.Con
             .addOnConnectionFailedListener(this)
             .addApi(LocationServices.API)
             .build();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
@@ -111,24 +120,14 @@ public class PlacePickerActivity extends Activity implements GoogleApiClient.Con
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                NavUtils.navigateUpFromSameTask(this);
-                return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
+    protected void onResume() {
+        super.onResume();
         mGoogleApiClient.connect();
     }
 
     @Override
-    protected void onStop() {
-        super.onStop();
+    protected void onPause() {
+        super.onPause();
         mGoogleApiClient.disconnect();
     }
 
