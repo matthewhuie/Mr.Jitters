38aa87373b7fd84ea1e7a76963a026fa9e9b5cc5
Matthew Huie <mhuie@foursquare.com>
2016-11-20 21:03:13 -0500

    Removed API keys

--- app/src/main/java/com/matthewhuie/mrjitters/FoursquareResponse.java
+++ app/src/main/java/com/matthewhuie/mrjitters/FoursquareResponse.java
@@ -1,7 +1,11 @@
 package com.matthewhuie.mrjitters;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class FoursquareResponse {
 
     FoursquareGroup group;
+    List<FoursquareVenue> venues = new ArrayList<>();
 
 }
